cc4a80a8185a7fdc42e0c9fee3c1686027769ed1
Manoelcaires7 <manelcairess0707@gmail.com>
2025-08-13 19:37:48 -0300

    Adição do modelo Product e integração no controlador de produtos

--- src/app/controllers/productController.js
+++ src/app/controllers/productController.js
@@ -1,22 +1,31 @@
 import * as Yup from 'yup'
+import Product from '../models/product';
 class productController {
 
-    async store(req, res){
-    const schema = Yup.object({
-        name: Yup.string().required(),
-        price: Yup.number().required(),
-        category: Yup.string().required(),
-    });
+    async store(req, res) {
+        const schema = Yup.object({
+            name: Yup.string().required(),
+            price: Yup.number().required(),
+            category: Yup.string().required(),
+        });
 
-    try {
+        try {
             schema.validateSync(req.body, { abortEarly: false });
         } catch (err) {
             return res.status(400).json({ error: err.errors });
         };
 
-        return res.status(201).json({message: 'ok'})
+        const {filename: path} = req.file;
+        const { name, price, category } = req.body;
+        const product = await Product.create({
+            name,
+            price,
+            category,
+            path,
+        });
+
+        return res.status(201).json({product})
     }
 
 }
-
 export default new productController();
